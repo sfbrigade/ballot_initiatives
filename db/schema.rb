bf6c73aaecea37960dee0102bfa926ecc2f70a87
Matt Luedke <mluedke2@gmail.com>
2013-06-01 14:16:51 -0700

    correcting db issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130524204810) do
+ActiveRecord::Schema.define(:version => 20130601211035) do
 
   create_table "consultant_services", :force => true do |t|
     t.integer  "total_promised"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(:version => 20130524204810) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.integer  "initiative_id"
+  end
+
+  create_table "people", :force => true do |t|
+    t.string   "name"
+    t.string   "website"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
